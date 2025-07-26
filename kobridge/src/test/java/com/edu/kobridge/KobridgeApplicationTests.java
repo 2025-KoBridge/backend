7ad0d4ace87a924ac931279ffb6b40f6216db02c
yerim1ee <yerim1226@gmail.com>
2025-07-26 22:54:28 +0900

    :tada: feat: 기초 세팅 [KOBG-4]

---
+++ kobridge/src/test/java/com/edu/kobridge/KobridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.kobridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KobridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
